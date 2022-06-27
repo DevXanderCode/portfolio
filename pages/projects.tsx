15d2d015dd168693096fad84a80a5d981140552c
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-06-27 04:15:08 +0100

    animated the project detail section

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from "next";
 import { useState } from "react";
+import { motion } from "framer-motion";
 import { ProjectCard, ProjectsNavBar } from "../components";
 import { projects as projectsData } from "../data";
 import { Category, IProject } from "../types";
+import { fadeInUp, stagger } from "../animations";
 
 const Projects: NextPage = () => {
   const [projects, setProjects] = useState<IProject[]>(projectsData);
@@ -20,22 +22,29 @@ const Projects: NextPage = () => {
     );
     setProjects(filterProject);
   };
+
   return (
     <div className="px-5 py-2 overflow-y-scroll h-[90vh]">
       <ProjectsNavBar
         handleFilterHandler={handleFilterHandler}
         activeNav={active}
       />
-      <div className="relative grid grid-cols-12 gap-4 my-3">
+      <motion.div
+        className="relative grid grid-cols-12 gap-4 my-3"
+        variants={stagger}
+        initial="initial"
+        animate="animate"
+      >
         {projects?.map((project) => (
-          <div
+          <motion.div
             key={project?.name}
+            variants={fadeInUp}
             className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200"
           >
             <ProjectCard project={project} />
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
