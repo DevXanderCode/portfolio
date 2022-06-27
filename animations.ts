efed590af163c7a7acbab6d6b375ff32424828c8
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-06-27 03:45:21 +0100

    animated the education and experience section

---
+++ animations.ts
@@ -0,0 +1,16 @@
+export const fadeInUp = {
+  initial: {
+    opacity: 0,
+    y: 60,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+      type: "spring",
+      damping: 10,
+      stiffnes: 100,
+    },
+  },
+};
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,5 +1,7 @@
 import { NextPage } from "next";
+import { motion } from "framer-motion";
 import { Bar } from "../components";
+import { fadeInUp } from "../animations";
 import { languages, tools } from "../data";
 
 const Resume: NextPage = () => {
@@ -7,7 +9,7 @@ const Resume: NextPage = () => {
     <div className="px-6 py-2">
       {/* Education and Experience */}
       <div className="grid gap-6 md:grid-cols-2">
-        <div>
+        <motion.div variants={fadeInUp} initial="initial" animate="animate">
           <h5 className="my-2 text-2xl font-bold ">Education</h5>
           <div>
             <h5 className="my-1 text-xl font-bold">Mechanical Engineering</h5>
@@ -19,8 +21,8 @@ const Resume: NextPage = () => {
               text commonly used to In publishing and graphic design, Lorem
             </p>
           </div>
-        </div>
-        <div>
+        </motion.div>
+        <motion.div variants={fadeInUp} initial="initial" animate="animate">
           <h5 className="my-2 text-2xl font-bold ">Experience</h5>
           <div>
             <h5 className="my-1 text-xl font-bold">Lead Front End Developer</h5>
@@ -30,7 +32,7 @@ const Resume: NextPage = () => {
               this property tech start up based in Lagos Nigeria
             </p>
           </div>
-        </div>
+        </motion.div>
       </div>
 
       {/* Languages and Tools */}
